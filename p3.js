5d2863f70791f51edb66b53b0a076843cabce72f
zaidongy <zaidongy@gmail.com>
2018-10-13 22:26:40 -0700

    largest prime of large number

---
+++ p3.js
@@ -0,0 +1,19 @@
+// The prime factors of 13195 are 5, 7, 13 and 29.
+
+// What is the largest prime factor of the number 600851475143 ?
+
+var n = 600851475143;
+
+function findLPF(n) {
+    for(var i = 2; i < Math.sqrt(n); i++) {
+        if(n % i == 0) {
+            var result = n / i;  
+            console.log(`${n} / ${i} = ${result}`);
+            return findLPF(result);
+        }
+    }
+    console.log(`${n} is prime`); 
+    return n;
+}
+
+findLPF(n);
